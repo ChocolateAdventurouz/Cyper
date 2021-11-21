560b160b597f3b2301e392c36b99dd90caea5b94
SecurityRaven <92590193+SecurityRaven@users.noreply.github.com>
2021-11-21 21:07:20 +0200

    Add files via upload

---
+++ Cyper_proj/Cyper_proj/MainWindow.vb
@@ -0,0 +1,100 @@
+﻿Imports System.IO
+Imports System
+Public Class MainWindow
+    Private obj As Object
+
+    Private Sub SBootBtn_Click(sender As Object, e As EventArgs) Handles SBootBtn.Click
+        UseWaitCursor = 1
+        Process.Start("cmd", "bcdedit /set {default} bootmenupolicy legacy & taskkill /im cmd.exe & exit")
+        ActivityBar.Value = 100
+        MsgBox("The command has been executed. Please reboot your computer.")
+        ActivityBar.Value = 0
+        UseWaitCursor = 0
+    End Sub
+
+
+    Private Sub CDiskBtn_Click(sender As Object, e As EventArgs) Handles CDiskBtn.Click
+        UseWaitCursor = 1
+        Process.Start("cmd", "/k cleanmgr/ sagerun: 1 & taskkill /im cmd.exe")
+        ActivityBar.Value = 100
+        MsgBox("The command has been executed.")
+        ActivityBar.Value = 0
+        UseWaitCursor = 0
+    End Sub
+
+    Private Sub FreshBtn_Click(sender As Object, e As EventArgs) Handles FreshBtn.Click
+        UseWaitCursor = 1
+        Process.Start("cmd", "/k sfc /scannow & DISM /Online /Cleanup-Image /CheckHealth & DISM /Online /Cleanup-Image /ScanHealth & DISM /Online /Cleanup-Image /RestoreHealth /Source:repairSource\install.wim & taskkill /im cmd.exe")
+        ActivityBar.Value = 100
+        MsgBox("The command has been executed.")
+        ActivityBar.Value = 0
+        UseWaitCursor = 0
+    End Sub
+
+    Private Sub TempCleanBtn_Click(sender As Object, e As EventArgs) Handles TempCleanBtn.Click
+        UseWaitCursor = 1
+        Process.Start("cmd", "/k rd %temp% /s /q & md %temp% & cd C:\Windows\Temp & del *.* /Q & del C:\Windows\prefetch\*.*/s/q & taskkill /im cmd.exe")
+        ActivityBar.Value = 100
+        MsgBox("The command has been executed.")
+        ActivityBar.Value = 0
+        UseWaitCursor = 0
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        ScannerFDialog.ShowDialog()
+        Path.GetFileName(ScannerFDialog.FileName)
+
+        Dim fileName As String = Path.GetFileName(ScannerFDialog.FileName)
+        TextBox1.Clear()
+        TextBox1.Text = fileName
+        If TextBox1.Text = "" Then
+            TextBox1.Text = "_-_File Scanner + _-_"
+        End If
+        Dim NewProcess As New System.Diagnostics.Process()
+        With NewProcess.StartInfo
+            .FileName = "Scanner.exe"
+            .RedirectStandardOutput = True
+            .RedirectStandardError = True
+            .RedirectStandardInput = True
+            .UseShellExecute = False
+            .WindowStyle = ProcessWindowStyle.Normal
+            .CreateNoWindow = False
+        End With
+
+        NewProcess.Start()
+
+        System.Threading.Thread.Sleep(3000)
+        TextBox1.Text = NewProcess.StandardOutput.ReadToEnd
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub Label8_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub MainWindow_Click(sender As Object, e As EventArgs) Handles Me.Click
+
+    End Sub
+
+    Private Sub MainWindow_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        If e.KeyCode = Keys.F1 Then
+            About.Show()
+        End If
+    End Sub
+
+    Private Sub MainWindow_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub AboutCyperToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutCyperToolStripMenuItem.Click
+        About.Show()
+    End Sub
+End Class
+
---
+++ Cyper_proj/Cyper_proj/My Project/AssemblyInfo.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Resources
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Cyper")>
+<Assembly: AssemblyDescription("Built with ♥ by SecurityRaven                                                                                                         PicsArt: Logo creation :: Krita: Icon design :: RemixIcon & IconMonstr: Icon Library :: MalwareBazaar: Virus Database :: Python: Scanning engine")>
+<Assembly: AssemblyCompany("SecurityRaven")>
+<Assembly: AssemblyProduct("Cyper")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(True)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("7df2d1ed-4570-4477-b515-1d79b50f7e76")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: NeutralResourcesLanguage("en-US")>
